dde3007002e71ec4a281dcc1f64c40fb10ccecc7
Евгений Миськив <evgenymiskiv@gmail.com>
2022-09-28 13:35:23 +0300

    review 1 label, help-text

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -26,29 +26,28 @@ class PostModelTest(TestCase):
         self.assertEqual(str(PostModelTest.group), PostModelTest.group.title)
         self.assertEqual(str(PostModelTest.post), PostModelTest.post.text[:15])
 
-    def test_models_verbose_name(self):
-        """Проверяем, что у полей отображается нужный лейбл"""
-        field_verboses = {
-            'text': 'Текст поста',
-            'group': 'Группа',
+    def test_models_verbose_and_help_names(self):
+        """Проверяем, что у полей отображается нужные лейблы и подсказки"""
+        fields = {
+            'text': {
+                'verbose_name': 'Текст поста',
+                'help_text': 'Текст нового поста'
+            },
+            'group': {
+                'verbose_name': 'Группа',
+                'help_text': 'Группа, к которой будет относиться пост'
+            }
         }
-        for field, expected_value in field_verboses.items():
+
+        for field, expected_value in fields.items():
             with self.subTest(field=field):
                 self.assertEqual(
                     PostModelTest.post._meta.get_field(field).verbose_name,
-                    expected_value,
+                    expected_value['verbose_name'],
                     'Неверное имя поля'
                 )
-
-    def test_models_help_text(self):
-        """Проверяем, что у полей отображается нужный хелп-текст"""
-        field_help_texts = {
-            'text': 'Текст нового поста',
-            'group': 'Группа, к которой будет относиться пост'
-        }
-        for field, expected_value in field_help_texts.items():
-            with self.subTest(field=field):
                 self.assertEqual(
                     PostModelTest.post._meta.get_field(field).help_text,
-                    expected_value
+                    expected_value['help_text'],
+                    'Неверное имя подсказки'
                 )
